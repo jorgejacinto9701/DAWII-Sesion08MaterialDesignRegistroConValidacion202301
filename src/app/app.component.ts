97b2c268a33c8cfb5f04374062d557667a224642
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-05-16 02:48:40 -0500

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DAWII-Sesion08MaterialDesignRegistro202301';
+  title = 'DAWII-Sesion08MaterialDesignRegistroConValidacion202301';
 }
