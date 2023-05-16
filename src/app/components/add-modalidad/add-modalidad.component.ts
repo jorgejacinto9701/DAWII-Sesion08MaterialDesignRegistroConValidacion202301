bfe6bae32004dcc528e22dd98e9fffb7aef6b156
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-05-16 02:29:09 -0500

    validaciones

--- src/app/components/add-modalidad/add-modalidad.component.ts
+++ src/app/components/add-modalidad/add-modalidad.component.ts
@@ -4,6 +4,7 @@ import { Deporte } from 'src/app/models/deporte.model';
 import { Modalidad } from 'src/app/models/modalidad.model';
 import { DeporteService } from 'src/app/services/deporte.service';
 import { ModalidadService } from 'src/app/services/modalidad.service';
+import { Validators, FormBuilder } from "@angular/forms";
 import Swal from 'sweetalert2'
 
 @Component({
@@ -20,16 +21,30 @@ export class AddModalidadComponent implements OnInit {
     }
   };
 
-  constructor(private deporteService:DeporteService, private modalidadService: ModalidadService) { 
+  formsRegistra= this.formBuilder.group({
+    nombre:  ['', [Validators.required, Validators.pattern('[a-zA-ZáéíóúÁÉÍÓÚñ0-9 ]{3,30}')]],
+    numHombres: ['', [Validators.required, Validators.pattern('[0-9]{1,2}')]],
+    numMujeres: ['', [Validators.required, Validators.pattern('[0-9]{1,2}')]],
+    edadMinima: ['', [Validators.required, Validators.pattern('[0-9]{2}'), Validators.min(18), Validators.max(25)]],
+    edadMaxima: ['', [Validators.required, Validators.pattern('[0-9]{2}'), Validators.min(18), Validators.max(40), Validators.max(40)]],
+    sede: ['', [Validators.required, Validators.pattern('[a-zA-ZáéíóúÁÉÍÓÚñ0-9 ]{3,30}')]],
+    deporte: ['', [Validators.min(0)]],
+});
+
+
+
+  constructor(private formBuilder: FormBuilder,private deporteService:DeporteService, private modalidadService: ModalidadService) { 
       this.deporteService.listaDeporte().subscribe(
           x => this.lstDeportes = x
       );
   }
 
   registra(){
-        this.modalidadService.registraModalidad(this.objModalidad).subscribe(
-          x => Swal.fire({icon: 'info',title: 'Resultado del Registro',text: x.errores}) 
-        );
+      if (this.formsRegistra.valid) {
+            this.modalidadService.registraModalidad(this.objModalidad).subscribe(
+              x => Swal.fire({icon: 'info',title: 'Resultado del Registro',text: x.errores}) 
+            );
+      }
   }  
 
   ngOnInit(): void {
